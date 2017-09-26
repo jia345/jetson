9f5913407f8ab692fb94797c748602ca589c3f1d
Jia Xin <jia345@gmail.com>
2017-09-26 12:53:40 +0000

    initial mimic web server

---
+++ cloud.py
@@ -0,0 +1,3 @@
+# The purpose of this script is to provide a mimic web server in cloud
+# 1. dump the messages between web server and main controller in unmanned box
+# 2. interactively sending request or receiving response
