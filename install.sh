917994fb7eca94c9d60f4011ea9f83ef2cffab33
Jia Xin <jia345@gmail.com>
2017-09-27 07:22:03 +0000

    fix installation error when directory exists

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 pip install -r requirements.txt
-sudo mkdir /usr/local/unmanned-counter
+INSTALL_DIR=/usr/local/unmanned-counter
+[ -d ${INSTALL_DIR} ] || sudo mkdir ${INSTALL_DIR}
 sudo cp *.py /usr/local/unmanned-counter
+echo "\n=========================================="
+echo "    unmanned-counter is ready to run"
+echo "==========================================\n"
