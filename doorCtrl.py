b553d3e65d44bee5eaeaac4b8ab2400189567516
ZhuKe423 <mczk423@163.com>
2017-10-08 09:41:35 +0800

    add the door controller

--- doorCtrl.py
+++ doorCtrl.py
@@ -5,29 +5,41 @@ from twisted.internet import reactor
 import time
 
 
-def door_is_closed(pin, state):
-    dateTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-    print format("the door is closed !!! at %s " % dateTime)
-    print format("pin = %d state = %d" % (pin,state))
+OPEN_THE_DOOR_GPIO  =  186
+CHECK_THE_DOOR_GPIO =  187
 
-OPEN_THE_DOOR=186
-CHECK_THE_DOOR=187
+DOOR_IN_OPEN  = True
+DOOR_IN_CLOSE = False
 
- class DoorCtrl():
-    def __init__(self):
-        Controller.available_pins = [OPEN_THE_DOOR,CHECK_THE_DOOR]
-        Controller.alloc_pin(number=OPEN_THE_DOOR, direction='out')
-        Controller.alloc_pin(number=CHECK_THE_DOOR, direction='in',callback=door_is_closed,edge='falling')
+class DoorCtrl():
+    def __init__(self,cb_door_close=None):
+        self.door_closed_cb = cb_door_close
+        self.door_state = DOOR_IN_CLOSE
+        Controller.available_pins = [OPEN_THE_DOOR_GPIO,CHECK_THE_DOOR_GPIO]
+        Controller.alloc_pin(number=OPEN_THE_DOOR_GPIO, direction='out')
+        Controller.alloc_pin(number=CHECK_THE_DOOR_GPIO, direction='in',callback=self.door_is_closed,edge='falling')
+
+    def door_is_closed(self,pin, state):
+        dateTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        print format("the door is closed !!! at %s " % dateTime)
+        print format("pin = %d state = %d" % (pin, state))
+        self.door_state = DOOR_IN_CLOSE
+
+        if self.door_closed_cb != None :
+            self.door_closed_cb()
 
     def open_the_door(self):
-        Controller.set_pin(OPEN_THE_DOOR)
+        Controller.set_pin(OPEN_THE_DOOR_GPIO)
         dateTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         print format("the door is opend !!! at %s " % dateTime)
         sleep(2)
-        Controller.reset_pin(OPEN_THE_DOOR)
+        Controller.reset_pin(OPEN_THE_DOOR_GPIO)
+        self.door_state = DOOR_IN_OPEN
 
     def check_the_door(self):
-        return Controller.get_pin_state(CHECK_THE_DOOR)
+        state = Controller.get_pin_state(CHECK_THE_DOOR_GPIO)
+
+        return self.door_state
 
 
 
