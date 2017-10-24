55346825a461bdc440d73d1734ac530a6354ccd4
ZhuKe423 <mczk423@163.com>
2017-10-24 18:01:11 +0800

    add the usb 4G modem ctrl

--- jetson_boot.sh
+++ jetson_boot.sh
@@ -11,9 +11,3 @@
 ### END INIT INFO
 
 echo 'ubuntu' | sudo -S python /home/ubuntu/jetson.jia345/usbmodem4g.py &
-
-touch /tmp/jetpack.log
-echo 'ubuntu' | sudo -S python /home/ubuntu/jetson.jia345/jetson.py  > /home/ubuntu/badnews &
-echo "starting script to send ip to host" >> /tmp/jetpack.log
-/home/ubuntu/report_ip_to_host.sh &
-echo "started script to send ip to host" >> /tmp/jetpack.log
